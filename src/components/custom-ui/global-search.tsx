0cdc0d65bc89bb1fe700e20c886571f7ab106359
galibremo <galibremo@gmail.com>
2026-08-10 09:38:27 +0600

    feat: implement GlobalSearch component and integrate into HomeHeader layout

--- src/components/custom-ui/global-search.tsx
+++ src/components/custom-ui/global-search.tsx
@@ -2,12 +2,17 @@ import { Search } from "@hugeicons/core-free-icons"
 import { Input } from "../ui/input"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { Button } from "../ui/button"
+import { cn } from "@/lib/utils"
 
+interface GlobalSearchProps {
+    className?: string;
+    autoFocus?: boolean;
+}
 
-const GlobalSearch = () => {
+const GlobalSearch: React.FC<GlobalSearchProps> = ({ className, autoFocus }) => {
     return (
-        <div className="relative hidden md:flex items-center justify-center w-full max-w-xl">
-            <Input type="text" placeholder="Search" className="h-10 ring-0! px-4 pr-9 w-full" />
+        <div className={cn("relative items-center justify-center w-full", className)}>
+            <Input type="text" placeholder="Search" className={cn("h-10 ring-0! px-4 pr-9 w-full rounded-none lg:rounded-lg focus-visible:border-input lg:focus-visible:border-ring")} autoFocus={autoFocus} />
             <Button variant="secondary" className="absolute right-1.5 cursor-pointer" size="icon-sm">
                 <HugeiconsIcon icon={Search} />
             </Button>
